b04d7b0d5224795956010e5d43e9dd1aa1e54ef3
Sebadalpra <sebatiandalpra@gmail.com>
2025-04-22 15:10:37 -0300

    actualización

--- C/01.js
+++ C/01.js
@@ -1,15 +1,71 @@
-console.log("Ejercicio 01:")
-console.log("HOLA MUNDO!")
-console.log("Puedo mostrar comillas ´simples´")
-console.log("Puedo mostrar comillas \"dobles\"")
+console.log("Ejercicio 01:");
+console.log("HOLA MUNDO!");
+console.log("Puedo mostrar comillas ´simples´");
+console.log('Puedo mostrar comillas "dobles"');
 
-console.log("Ejercicio 02:")
+console.log("Ejercicio 02:");
 for (let i = 0; i < 20; i++) {
     if (i % 2 == 0) {
-        continue
+        continue;
+    } else if (i == 15) {
+        break;
     }
-    else if (i == 15) {
-        break
-    }
-    console.log(i)
+    console.log(i);
 }
+
+console.log("Ejercicio 03:");
+
+let meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+];
+for (let i = 0; i < meses.length; i++) {
+    console.log(meses[i] + " es el mes " + (i + 1));
+}
+
+console.log("Ejercicio 04:");
+let i = 0;
+function numeroCinco() {
+    numero = prompt("Introduce un número:");
+    if (numero == 5) {
+        console.log("El número 5 es impar.");
+    }
+}
+
+numeroCinco();
+
+console.log("Ejercicio 05:");
+function mostrar(numero, texto) {
+
+    if(typeof numero !== "number"){
+        console.log("El primer argumento no es un número.");
+        return;
+    }
+    
+    if(typeof texto == "string"){
+        for (let i = 0; i < numero; i++) {
+            console.log(texto);
+          }
+        } else {
+          console.log(1 / numero);
+        }
+}
+
+mostrar(3, "Hola"); 
+// Hola 3 veces
+
+mostrar(4); 
+// 0.25
+
+mostrar("no es un número", "texto");
+// El primer argumento no es un número.
